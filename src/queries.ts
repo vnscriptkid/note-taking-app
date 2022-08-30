7607583629782acb20b7ba958a7a85dee20f0abd
Thanh Nguyen <vnscriptkid@gmail.com>
2022-08-30 23:18:15 +0700

    feat(notes): markdown posts desc

--- src/queries.ts
+++ src/queries.ts
@@ -1,8 +1,12 @@
+import { marked } from "marked"
 import { Collection, Document } from "mongodb"
 
 export async function retrieveNotes(db: Collection<Document>) {
     const notes = (await db.find().toArray()).reverse()
-    return notes
+    return notes.map(it => ({
+        ...it,
+        description: marked(it.description)
+    }))
 }
 
 export async function saveNote(db: Collection<Document>, note: any) {
